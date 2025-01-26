6d98b9f1cf45348bc013c91e4ca6f81c3ad2b622
daalve <alcaidevela@gmail.com>
2025-01-26 18:10:05 +0100

    Cambiado port para render

--- server/app.js
+++ server/app.js
@@ -122,7 +122,8 @@ app.use((err, req, res, next) => {
 });
 
 // Iniciar el servidor
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
